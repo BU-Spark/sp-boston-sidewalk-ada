9f5b6eaf8384ba3fe0b3f603ea973da4112486f4
Zach Bodi <zachbodi@bu.edu>
2021-11-01 22:00:34 -0400

    added functionality for previewing the camera, recording, and playing recordings

---
+++ data_labeling/preview.py
@@ -0,0 +1,47 @@
+import cv2
+import numpy as np
+import pyrealsense2 as rs
+
+def main():
+    pipeline = rs.pipeline()
+    config = rs.config()
+
+    config.enable_stream(rs.stream.depth, 1280, 720, rs.format.z16, 30)
+    config.enable_stream(rs.stream.color, 1280, 720, rs.format.bgr8, 30)
+
+    pipeline.start(config)
+
+    try:
+        while True:
+            # Get the next available color and depth frames
+            frames = pipeline.wait_for_frames()
+            color_frame = frames.get_color_frame()
+            depth_frame = frames.get_depth_frame()
+
+            # Convert these frames to numpy arrays (representing images)
+            color_image = np.asanyarray(color_frame.get_data())
+            depth_image = np.asanyarray(depth_frame.get_data())
+
+            # Create a colormapped version of the depth image
+            depth_colormap = cv2.applyColorMap(cv2.convertScaleAbs(depth_image, alpha=0.03),
+                    cv2.COLORMAP_JET)
+
+            # If depth and color are different shapes, resize color. Then, horizontally stack them.
+            if depth_colormap.shape != color_image.shape:
+                resized_color_image = cv2.resize(color_image,
+                        dsize=(depth_colormap.shape[1], depth_colormap.shape[0]),
+                        interpolation=cv2.INTER_AREA)
+                images = np.hstack((resized_color_image, depth_colormap))
+            else:
+                images = np.hstack((color_image, depth_colormap))
+
+            # Display the frame and wait for a key press
+            cv2.namedWindow('RealSense', cv2.WINDOW_AUTOSIZE)
+            cv2.imshow('RealSense', images)
+            cv2.waitKey(1)
+    finally:
+        pipeline.stop()
+
+if __name__ == '__main__':
+    main()
+
---
+++ data_labeling/record.py
@@ -0,0 +1,32 @@
+from datetime import datetime
+import pyrealsense2 as rs
+
+def main(argv):
+    pipeline = rs.pipeline()
+    config = rs.config()
+
+    config.enable_stream(rs.stream.depth, 1280, 720, rs.format.z16, 30)
+    config.enable_stream(rs.stream.color, 1280, 720, rs.format.bgr8, 30)
+
+    filename = datetime.now().strftime('%m-%d-%Y_%H:%M:%S') + '.bag'
+    config.enable_record_to_file(filename)
+
+    pipeline.start(config)
+
+    try:
+        frame_num = 0
+        while True:
+            # Get the next available color and depth frames
+            frames = pipeline.wait_for_frames()
+            color_frame = frames.get_color_frame()
+            depth_frame = frames.get_depth_frame()
+
+            print('received frame {frame_num}')
+            frame_num += 1
+
+    finally:
+        pipeline.stop()
+
+if __name__ == '__main__':
+    main()
+
