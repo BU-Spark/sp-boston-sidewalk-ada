a82dfca52840a367e7dec80ffe7b90ebd2316d57
Zach Bodi <zachbodi@bu.edu>
2021-11-11 14:16:49 -0500

    bug fixes; added support for recording and playing IMU data

--- data_labeling/label.py
+++ data_labeling/label.py
@@ -1,31 +1,33 @@
 import cv2
 import numpy as np
+import os
 import pyrealsense2 as rs
 import sys
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+ACCESSIBLE_DIR = os.path.join(BASE_DIR, 'data/accessible')
+INACCESSIBLE_DIR = os.path.join(BASE_DIR, 'data/inaccessible')
 
 def main(argv):
     config = rs.config()
     rs.config.enable_device_from_file(config, argv[0])
     config.enable_stream(rs.stream.depth, 1280, 720, rs.format.z16, 30)
     config.enable_stream(rs.stream.color, 1280, 720, rs.format.bgr8, 30)
-    config.enable_stream(rs.stream.accel, rs.format.motion_xyz32f, 250)
-    config.enable_stream(rs.stream.gyro, rs.format.motion_xyz32f, 200)
 
     pipeline = rs.pipeline()
     pipeline.start(config)
 
     try:
+        frame_num = 0 
         while True:
             # Get the next available color and depth frames
             frames = pipeline.wait_for_frames()
             color_frame = frames.get_color_frame()
             depth_frame = frames.get_depth_frame()
-            motion_frame = frame.as_motion_frame()
 
             # Convert these frames to numpy arrays (representing images)
             color_image = np.asanyarray(color_frame.get_data())
             depth_image = np.asanyarray(depth_frame.get_data())
-            motion_data = np.asanyarray(motion_frame.get_motion_data())
 
             # Create a colormapped version of the depth image
             depth_colormap = cv2.applyColorMap(cv2.convertScaleAbs(depth_image, alpha=0.03),
@@ -41,12 +43,25 @@ def main(argv):
                 images = np.hstack((color_image, depth_colormap))
 
             # Display the frame and wait for a key press
-            print(motion_data)
             cv2.namedWindow('RealSense', cv2.WINDOW_AUTOSIZE)
             cv2.imshow('RealSense', images)
-            cv2.waitKey(1)
+            pressed_key = chr(cv2.waitKey(0))
+
+            # 1 = label as accessible, 2 = label as inaccessible, ESC = quit
+            if pressed_key == '1':
+                cv2.imwrite(os.path.join(ACCESSIBLE_DIR, f'{argv[0]}_color_{frame_num}.jpeg'), color_image)
+                cv2.imwrite(os.path.join(ACCESSIBLE_DIR, f'{argv[0]}_depth_{frame_num}.jpeg'), depth_image)
+            elif pressed_key == '2':
+                cv2.imwrite(os.path.join(INACCESSIBLE_DIR, f'{argv[0]}_color_{frame_num}.jpeg'), color_image)
+                cv2.imwrite(os.path.join(INACCESSIBLE_DIR, f'{argv[0]}_depth_{frame_num}.jpeg'), depth_image)
+            elif pressed_key == chr(27): #ESC
+                break
+            
+            frame_num += 1
+
     finally:
         pipeline.stop()
 
 if __name__ == '__main__':
     main(sys.argv[1:])
+
--- data_labeling/record.py
+++ data_labeling/record.py
@@ -1,12 +1,14 @@
 from datetime import datetime
 import pyrealsense2 as rs
 
-def main(argv):
+def main():
     pipeline = rs.pipeline()
     config = rs.config()
 
     config.enable_stream(rs.stream.depth, 1280, 720, rs.format.z16, 30)
     config.enable_stream(rs.stream.color, 1280, 720, rs.format.bgr8, 30)
+    config.enable_stream(rs.stream.accel, rs.format.motion_xyz32f, 250)
+    config.enable_stream(rs.stream.gyro, rs.format.motion_xyz32f, 200)
 
     filename = datetime.now().strftime('%m-%d-%Y_%H:%M:%S') + '.bag'
     config.enable_record_to_file(filename)
@@ -21,7 +23,7 @@ def main(argv):
             color_frame = frames.get_color_frame()
             depth_frame = frames.get_depth_frame()
 
-            print('received frame {frame_num}')
+            print(f'received frame {frame_num}')
             frame_num += 1
 
     finally:
